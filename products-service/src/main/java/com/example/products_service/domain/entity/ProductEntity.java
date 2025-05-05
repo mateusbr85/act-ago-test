96b22af988176d0fb96fbe4367be327fb9340e44
mvcode <mateus.13061903@gmail.com>
2025-05-04 23:11:19 -0300

    feat: adicionando novo service

---
+++ products-service/src/main/java/com/example/products_service/domain/entity/ProductEntity.java
@@ -0,0 +1,18 @@
+package com.example.products_service.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductEntity {
+
+}
