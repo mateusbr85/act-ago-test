d483a6692fb8dd68e7061ee44d58f8d749641b40
mvcode <mateus.13061903@gmail.com>
2025-05-05 22:06:07 -0300

    fix: Ajustando a serealização para o kafka corretamente

--- api-act-orders/src/main/java/com/example/api_act_orders/config/KafkaProducerConfig.java
+++ api-act-orders/src/main/java/com/example/api_act_orders/config/KafkaProducerConfig.java
@@ -2,7 +2,7 @@ package com.example.api_act_orders.config;
 
 import com.example.api_act_orders.domain.record.OrderCreatedEvent;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,7 +29,6 @@ public class KafkaProducerConfig {
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapAddress);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
         return new DefaultKafkaProducerFactory<>(config);
     }
 
